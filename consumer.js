b1f6dc7e5b22a38dff25ad58d0279ce70e9a8dec
anix001 <chaudharyanish305@gmail.com>
2024-12-03 12:33:27 +0545

    Feat: Delayed queue

--- consumer.js
+++ consumer.js
@@ -1,29 +1,40 @@
 const amqp = require("amqplib");
 
-async function receiveMail(){
+async function receiveMessage(){
  try{
   
         //creating a connection
         const connection = await amqp.connect("amqp://localhost");
-
         //creating a channel
         const channel = await connection.createChannel();
 
-        //creating a queue
-        await channel.assertQueue("mail_queue", {durable: false});
+         // Declare an exchange and queue
+         const exchange = 'delayed_exchange';
+         const queue = 'delayed_queue';
 
+         await channel.assertExchange(exchange, 'x-delayed-message', {
+            durable: true,
+            arguments: { 'x-delayed-type': 'direct' },
+          });
+      
+          await channel.assertQueue(queue, { durable: true });
+      
+          // Bind the queue to the exchange with a routing key
+          await channel.bindQueue(queue, exchange, 'delayed_key');
+      
+          console.log('Waiting for messages...');
         //consuming message
-        channel.consume("mail_queue", (message)=>{
+        channel.consume(queue, (message)=>{
             if(message !== null){
-                console.log("Message Received :", JSON.parse(message.content));
+                console.log("Message Received :", message.content.toString());
 
                 //acknowledge the message
                 channel.ack(message);
             }
-        })
+        },  { noAck: false });
  }catch(err){
     console.log("[Error] received : ", err);
  }
 }
 
-receiveMail();
+receiveMessage();
--- producer.js
+++ producer.js
@@ -1,47 +1,34 @@
   const amqp = require("amqplib");
 
-  async function sendMail(){
+  async function sendMessage(){
     try{
 
         //creating a connection
         const connection = await amqp.connect("amqp://localhost");
-
         //creating a channel
         const channel = await connection.createChannel();
 
         //creating an exchange name
-        const exchange = "mail_exchange";
-
-        //creating a routing_key
-        const routingKey  ="send_mail";
-
-        const message = {
-            to:"anishc381@gmail.com",
-            from:"barcafan830@gmail.com",
-            subject:"Mail Testing",
-            body:"Hello from mail server!!"
-        }
-        
+        const exchange = "delayed_exchange";     
+        const routingKey ="delayed_key";  
          //creating an exchange
-        await channel.assertExchange(exchange, "direct", {durable:false});
-        //creating a queue
-        await channel.assertQueue("mail_queue", {durable: false});
+        await channel.assertExchange(exchange, "x-delayed-messagge", {durable:true, arguments:{'x-delayed-type':'direct'}});
 
-        //binding exchange with a queue
-        await channel.bindQueue("mail_queue", exchange, routingKey);
+        const message = 'Hello, Delayed Queue!';
+        const delay = 5000; // Delay in milliseconds (5 seconds)
 
         //sending message to queue using exchange and routing key
-        channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)));
-        console.log("Mail data was sent", message);
+        channel.publish(exchange, routingKey, Buffer.from(message), {
+          headers: { 'x-delay': delay },
+        });
 
-        //closing the connection
-        setTimeout(()=>{
-            connection.close();
-        },500)
+        console.log(`Message sent: "${message}" with delay: ${delay}ms`);
+        await channel.close();
+        await connection.close();
 
     }catch(err){
       console.log("Error", err);
     }
   }
 
-  sendMail();
+  sendMessage();
